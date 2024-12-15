9169248a1a54d387b4ec2a4f1a4b3928569341b3
MHamzaFarooq <hamzafarooq49ml@gmail.com>
2024-12-15 16:34:47 +0500

    sidebar completed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -6,13 +6,13 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="w-[330px] h-full p-9 flex justify-center border-r border-r-[rgba(255,255,255,0.1)]">
-      <div className="w-[258px] h-full">
+    <div className="w-[330px] p-9 flex justify-center fixed left-0 bottom-0 top-0 border-r border-r-[rgba(255,255,255,0.1)]">
+      <div className="w-[258px] flex flex-col h-full">
         <div className="flex items-center gap-4 mb-[72px]">
           <LogoSVG />
-          <div className="text-white block">lmaofaggot</div>
+          <div className="text-white block">Syncode</div>
         </div>
-        <div className="h-full flex flex-col">
+        <div className="flex-1 flex flex-col justify-between">
           <div>
             <div className="flex items-center gap-4 border border-[rgba(255,255,255,0.1)] hover:border-[rgba(255,255,255,0.35)] p-4 rounded-[12px] bg-[#0F0E10] cursor-pointer">
               <HomeBtn />
